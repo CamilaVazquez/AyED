d7c7f4203ddbdee065e74f17a55749e30fe7eb42
CamilaVazquez <135062362+CamilaVazquez@users.noreply.github.com>
2023-06-08 21:35:26 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+
+int pi;
+int it=0;
+int nominador=-4;
+int denominador=1;
+
+
+    for (pi=0; pi==3.141592; it++)
+    {
+        pi=(nominador/denominador);
+        nominador=-(nominador);
+        denominador= denominador + 2;
+    }
+
+cout << "el valor de pi es " << pi << endl;
+cout << "Se realizaron " << it << " iteraciones" << endl;
+
+return 0;
+}
